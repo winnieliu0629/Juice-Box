e8de968b528caf8730fa37ec18a137e1120d3279
Winnie Liu <pewinonlyy@gmail.com>
2023-01-26 18:52:54 -0800

    fix deployment issue

--- db/index.js
+++ db/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+console.log(process.env.DATABASE_URL)
 const { Client } = require('pg'); // imports the pg module
 
 // supply the db name and location of the database
 const client = new Client({
-    connectionString: process.env.DATABASE_URL || 'postgres://juice_box_database_user:BhaWVm8YHzAvqRXVuE4byUyLQfK03uBT@dpg-cf7mo3en6mplr477lnng-a.oregon-postgres.render.com/juice_box_database',
+    connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 });
 
